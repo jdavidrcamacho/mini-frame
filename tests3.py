f2f349ae727e457e30dd222b33c657cedd86ba05
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-03-07 15:55:09 +0000

    more tests

--- tests3.py
+++ tests3.py
@@ -60,32 +60,32 @@ yerr=np.hstack((rvyerr,sig_rhk,np.random.uniform(2,2.5)*rvyerr))
 ##print
 
 #### simple sample and marginalization with emcee
-runs, burns = 10000, 10000
+runs, burns = 25000, 25000
 if kernel == 2:
     #probabilistic model
     def logprob(p):
         #print np.exp(p)
-        if any([p[0] < -10, p[0] > 10, 
+        if any([p[0] < -10, p[0] > 1, 
                 p[1] < -10, p[1] > 10,
                 p[2] < -10, p[2] > np.log(100),
                 p[3] < -10, p[3] > np.log(100),
                 p[4] < -10, p[4] > 10,
                 p[5] < -10, p[5] > 10,
-                p[6] < -10, p[6] > 10,
-                p[7] < -10, p[7] > 10]):
+                p[6] < -10, p[6] > np.log(20),
+                p[7] < -10, p[7] > np.log(10)]):
             return -np.inf
         logprior = 0.0
         return logprior + likelihood(kernel,p,t,y,yerr)
         
-    lp_prior = stats.uniform(np.exp(-10), np.exp(10) -np.exp(-10))          #[exp(-10) to exp(10)]
+    lp_prior = stats.uniform(np.exp(-10), np.exp(1) -np.exp(-10))          #[exp(-10) to exp(1)]
     le_prior = stats.uniform(np.exp(-10), np.exp(10) -np.exp(-10))          #[exp(-10) to exp(10)]
     p_prior = stats.uniform(np.exp(-10), 100 -np.exp(-10))                  #[exp(-10) to 100]
     vc_prior = stats.uniform(np.exp(-10), 100 -np.exp(-10))                 #[exp(-10) to 100]
 
     vr_prior = stats.uniform(np.exp(-10), np.exp(10) -np.exp(-10))         #[exp(-10) to 10]
     lc_prior = stats.uniform(np.exp(-10), np.exp(10)  -np.exp(-10))          #[exp(-10) to exp(10)]
-    bc_prior = stats.uniform(np.exp(-10), np.exp(10)  -np.exp(-10))          #[exp(-10) to 100]
-    br_prior = stats.uniform(np.exp(-10), np.exp(10)  -np.exp(-10))         #[exp(-10) to 100]
+    bc_prior = stats.uniform(np.exp(-10), 20 -np.exp(-10))          #[exp(-10) to 20]
+    br_prior = stats.uniform(np.exp(-10), 10  -np.exp(-10))         #[exp(-10) to 10]
     def from_prior():           
         return np.array([ lp_prior.rvs(), le_prior.rvs(), p_prior.rvs(),vc_prior.rvs(),
                          vr_prior.rvs(),lc_prior.rvs(),bc_prior.rvs(),br_prior.rvs() ])
@@ -105,32 +105,32 @@ if kernel == 2:
     
     print("Running production chain")
     sampler.run_mcmc(p0, runs);
-
+    
     #graphs
     print('graphics')
     fig, axes = pl.subplots(8, 1, sharex=True, figsize=(8, 9))
     axes[0].plot(np.exp(sampler.chain[:, burns:, 0]).T, color="k", alpha=0.4)
     axes[0].yaxis.set_major_locator(MaxNLocator(5))
     axes[0].set_ylabel("$lp$")
-    axes[1].plot(np.exp(sampler.chain[:, burns:, 0]).T, color="k", alpha=0.4)
+    axes[1].plot(np.exp(sampler.chain[:, burns:, 1]).T, color="k", alpha=0.4)
     axes[1].yaxis.set_major_locator(MaxNLocator(5))
     axes[1].set_ylabel("$le$")
-    axes[2].plot(np.exp(sampler.chain[:, burns:, 0]).T, color="k", alpha=0.4)
+    axes[2].plot(np.exp(sampler.chain[:, burns:, 2]).T, color="k", alpha=0.4)
     axes[2].yaxis.set_major_locator(MaxNLocator(5))
     axes[2].set_ylabel("$period$")
-    axes[3].plot(sampler.chain[:, burns:, 1].T, color="k", alpha=0.4) #log
+    axes[3].plot(np.exp(sampler.chain[:, burns:, 3]).T, color="k", alpha=0.4) #log
     axes[3].yaxis.set_major_locator(MaxNLocator(5))
     axes[3].set_ylabel("$Vc$")
-    axes[4].plot(sampler.chain[:, burns:, 2].T, color="k", alpha=0.4) #log
+    axes[4].plot(np.exp(sampler.chain[:, burns:, 4]).T, color="k", alpha=0.4) #log
     axes[4].yaxis.set_major_locator(MaxNLocator(5))
     axes[4].set_ylabel("$Vr$")
-    axes[5].plot(sampler.chain[:, burns:, 3].T, color="k", alpha=0.4) #log 
+    axes[5].plot(np.exp(sampler.chain[:, burns:, 5]).T, color="k", alpha=0.4) #log 
     axes[5].yaxis.set_major_locator(MaxNLocator(5))
     axes[5].set_ylabel("$Lc$")
-    axes[6].plot(sampler.chain[:, burns:, 4].T, color="k", alpha=0.4) #log
+    axes[6].plot(np.exp(sampler.chain[:, burns:, 6]).T, color="k", alpha=0.4) #log
     axes[6].yaxis.set_major_locator(MaxNLocator(5))
     axes[6].set_ylabel("$Bc$")
-    axes[7].plot(sampler.chain[:, burns:, 5].T, color="k", alpha=0.4) #log
+    axes[7].plot(np.exp(sampler.chain[:, burns:, 7]).T, color="k", alpha=0.4) #log
     axes[7].yaxis.set_major_locator(MaxNLocator(5))
     axes[7].set_ylabel("$Br$")
     axes[7].set_xlabel("step number")
@@ -142,13 +142,13 @@ if kernel == 2:
     samples = sampler.chain[:, burnin:, :].reshape((-1, ndim))
     
     samples[:, 0] = np.exp(samples[:, 0])   #lp
-    samples[:, 0] = np.exp(samples[:, 0])   #le
-    samples[:, 0] = np.exp(samples[:, 0])   #Period
-    samples[:, 1] = np.exp(samples[:, 1])   #Vc
-    samples[:, 2] = np.exp(samples[:, 2])   #Vr
-    samples[:, 3] = np.exp(samples[:, 3])   #Lc
-    samples[:, 4] = np.exp(samples[:, 4])   #Bc
-    samples[:, 5] = np.exp(samples[:, 5])   #Br
+    samples[:, 1] = np.exp(samples[:, 1])   #le
+    samples[:, 2] = np.exp(samples[:, 2])   #Period
+    samples[:, 3] = np.exp(samples[:, 3])   #Vc
+    samples[:, 4] = np.exp(samples[:, 4])   #Vr
+    samples[:, 5] = np.exp(samples[:, 5])   #Lc
+    samples[:, 6] = np.exp(samples[:, 6])   #Bc
+    samples[:, 7] = np.exp(samples[:, 7])   #Br
     
     lp_mcmc,le_mcmc,p_mcmc,vc_mcmc,vr_mcmc,lc_mcmc,bc_mcmc,br_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                                  zip(*np.percentile(samples, [16, 50, 84],axis=0)))
