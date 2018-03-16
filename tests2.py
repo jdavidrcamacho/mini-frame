6a5ef330b60796544b9c0224c203e76a521bcdfd
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-03-16 16:41:24 +0000

    tests

--- tests2.py
+++ tests2.py
@@ -81,9 +81,6 @@ if kernel == 1:
     p0[:,3] = np.random.uniform(np.exp(-10), np.exp(100), nwalkers)        # lc
     p0[:,4] = np.random.uniform(np.exp(-10), np.exp(100), nwalkers)        # bc
     p0[:,5] = np.random.uniform(np.exp(-10), np.exp(100), nwalkers)        # vr
-    p0[:,6] = np.random.uniform(np.exp(-10), np.exp(100), nwalkers)        # s1
-    p0[:,7] = np.random.uniform(np.exp(-10), np.exp(100), nwalkers)        # s2
-    p0[:,8] = np.random.uniform(np.exp(-10), np.exp(100), nwalkers)        # s3
     #Make sure we didn't by change pick an value that was too big
     p0[:,0] = np.minimum(p0[:,0], np.exp(100))
     p0[:,1] = np.minimum(p0[:,1], np.exp(100))
@@ -91,9 +88,6 @@ if kernel == 1:
     p0[:,3] = np.minimum(p0[:,3], np.exp(100))
     p0[:,4] = np.minimum(p0[:,4], np.exp(100))
     p0[:,5] = np.minimum(p0[:,5], np.exp(100))
-    p0[:,6] = np.minimum(p0[:,6], np.exp(100))
-    p0[:,7] = np.minimum(p0[:,7], np.exp(100))
-    p0[:,8] = np.minimum(p0[:,8], np.exp(100))
 
 
     print("Running burn-in")
@@ -137,11 +131,9 @@ if kernel == 1:
     samples[:, 3] = np.exp(samples[:, 3])   #Lc
     samples[:, 4] = np.exp(samples[:, 4])   #Bc
     samples[:, 5] = np.exp(samples[:, 5])   #Br
-    samples[:, 6] = np.exp(samples[:, 6])   #Lc
-    samples[:, 7] = np.exp(samples[:, 7])   #Bc
-    samples[:, 8] = np.exp(samples[:, 8])   #Br
 
-    l_mcmc,vc_mcmc,vr_mcmc,lc_mcmc,bc_mcmc,br_mcmc,s1_mcmc,s2_mcmc,s3_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+
+    l_mcmc,vc_mcmc,vr_mcmc,lc_mcmc,bc_mcmc,br_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                                  zip(*np.percentile(samples, [16, 50, 84],axis=0)))
     
     print('kernel length scale = {0[0]} +{0[1]} -{0[2]}'.format(l_mcmc))
@@ -150,6 +142,3 @@ if kernel == 1:
     print('Lc = {0[0]} +{0[1]} -{0[2]}'.format(lc_mcmc))
     print('Bc = {0[0]} +{0[1]} -{0[2]}'.format(bc_mcmc))
     print('Br = {0[0]} +{0[1]} -{0[2]}'.format(br_mcmc))
-    print('1 = {0[0]} +{0[1]} -{0[2]}'.format(s1_mcmc))
-    print('2 = {0[0]} +{0[1]} -{0[2]}'.format(s2_mcmc))
-    print('3 = {0[0]} +{0[1]} -{0[2]}'.format(s3_mcmc))
