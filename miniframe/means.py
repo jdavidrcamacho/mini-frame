55a66ff98a2afa1854b75ffcc8f7c38ba1a88a30
João Faria <joao.faria@astro.up.pt>
2018-03-29 16:01:56 +0100

    reimplement the mean functions

--- miniframe/means.py
+++ miniframe/means.py
@@ -1,20 +1,8 @@
 import numpy as np
-
-from inspect import signature
 from functools import wraps
- 
-def auto_args(f):
-    sig = signature(f)  # Get a signature object for the target:
-    def replacement(self, *args, **kwargs):
-        # Parse the provided arguments using the target's signature:
-        bound_args = sig.bind(self, *args, **kwargs)
-        # Save away the arguments on `self`:
-        for k, v in bound_args.arguments.items():
-            if k != 'self':
-                setattr(self, k, v)
-        # Call the actual constructor for anything else:
-        f(self, *args, **kwargs)
-    return replacement
+
+
+__all__ = ['Constant', 'Linear', 'Parabola', 'Keplerian']
 
 
 def array_input(f):
@@ -34,30 +22,44 @@ def array_input(f):
 #   def __call__(self, t):
 #       # Evaluate this mean function at array of times t
 
+class MeanModel(object):
+    _parsize = 0
 
-class Constant(object):
+    def __init__(self, *pars):
+        self.pars = list(pars)
+
+    def __repr__(self):
+        """ Representation of each instance """
+        return "{0}({1})".format(self.__class__.__name__,
+                                 ", ".join(map(str, self.pars)))
+    @classmethod
+    def initialize(cls):
+        return cls( *([0.]*cls._parsize) )
+
+
+class Constant(MeanModel):
     """ A constant offset mean function """
-    @auto_args
+    _parsize = 1
     def __init__(self, c):
-        pass
+        super(Constant, self).__init__(c)
 
     @array_input
     def __call__(self, t):
-        return self.c * np.ones_like(t)
+        return self.pars[0] * np.ones_like(t)
 
 
-class Linear(object):
+class Linear(MeanModel):
     """ 
     A linear mean function
     m(t) = slope * t + intercept 
     """
-    @auto_args
+    _parsize = 2
     def __init__(self, slope, intercept):
-        pass
+        super(Linear, self).__init__(slope, intercept)
 
     @array_input
     def __call__(self, t):
-        return self.slope * t + self.intercept
+        return self.pars[0] * t + self.pars[1]
 
 
 class Parabola(object):
@@ -65,39 +67,55 @@ class Parabola(object):
     A 2nd degree polynomial mean function
     m(t) = quad * t**2 + slope * t + intercept 
     """
-    @auto_args
+    _parsize = 2
     def __init__(self, quad, slope, intercept):
         pass
 
     @array_input
     def __call__(self, t):
-        return self.quad * t**2 + self.slope * t + self.intercept
+        return self.pars[0] * t**2 + self.pars[1] * t + self.pars[2]
 
 
-class Polynomial(object):
-    """ 
-    An N degree polynomial mean function
-    m(t) = coeff[0]*t**(N-1) + coeff[1]*t**(N-2) + ... + coeff[N-2]*t + coeff[N-1]
+class Keplerian(MeanModel):
     """
-    @auto_args
-    def __init__(self, coeff):
-        self.degree = len(coeff) - 1
+    Keplerian function
+    tan[phi(t) / 2 ] = sqrt(1+e / 1-e) * tan[E(t) / 2] = true anomaly
+    E(t) - e*sin[E(t)] = M(t) = eccentric anomaly
+    M(t) = (2*pi*t/tau) + M0 = Mean anomaly
+
+    P  = period in days
+    e = eccentricity
+    K = RV amplitude in m/s 
+    w = longitude of the periastron
+    T0 = zero phase
+    
+    RV = K[cos(w+v) + e*cos(w)] + sis_vel
+    """
+    _parsize = 5
+
+    def __init__(self, P, K, e, w, T0):
+        super(Keplerian, self).__init__(P, K, e, w, T0)
 
     @array_input
     def __call__(self, t):
-        return np.polyval(self.coeff, t)
+        P, K, e, w, T0 = self.pars
+        #mean anomaly
+        Mean_anom=[2*np.pi*(x1-T0)/P  for x1 in t]
+        #eccentric anomaly -> E0=M + e*sin(M) + 0.5*(e**2)*sin(2*M)
+        E0=[x1 + e*np.sin(x1)  + 0.5*(e**2)*np.sin(2*x1) \
+                                                            for x1 in Mean_anom]
+        #mean anomaly -> M0=E0 - e*sin(E0)
+        M0=[x1 - e*np.sin(x1) for x1 in E0]
 
-
-
-class Keplerian(object):
-    """
-    A Keplerian mean function
-    Parameters are the orbital period P, semi-amplitude K, eccentricity e,
-    argument of the periastron w, and time of periastron T0
-    """
-    @auto_args
-    def __init__(self, P, K, e, w, T0):
-        pass
-
-    @array_input(self, t):
-        pass
+        i=0
+        while i<100:
+            #[x + y for x, y in zip(first, second)]
+            calc_aux=[x2-y2 for x2,y2 in zip(Mean_anom,M0)]
+            E1=[x3 + y3/(1-e*np.cos(x3)) for x3,y3 in zip(E0,calc_aux)]
+            M1=[x4 - e*np.sin(x4) for x4 in E0]
+            i+=1
+            E0=E1
+            M0=M1
+        nu = [2*np.arctan(np.sqrt((1+e)/(1-e))*np.tan(x5/2)) for x5 in E0]
+        RV = [K*(e*np.cos(w)+np.cos(w+x6)) for x6 in nu]
+        return RV
