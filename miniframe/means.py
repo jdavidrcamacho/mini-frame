25ac1b5f1ef4a7b0c48579ddd3dd1602e95c84bc
João Faria <joao.faria@astro.up.pt>
2018-04-03 23:23:57 +0100

    make Parabola a MeanModel; test Sum of mean functions; improve Keplerian (why all the list comprehensions)

--- miniframe/means.py
+++ miniframe/means.py
@@ -3,23 +3,14 @@ from functools import wraps
 
 __all__ = ['Constant', 'Linear', 'Parabola', 'Keplerian']
 
-
 def array_input(f):
+    """ decorator to provide the __call__ methods with an array """
     @wraps(f)
     def wrapped(self, t):
         t = np.atleast_1d(t)
         r = f(self, t)
         return r
     return wrapped
-
-
-# class MeanModelTemplate(object):
-#   """ Mean functions should follow this strucuture """
-#   @auto_args
-#   def __init__(self, par1, par2, par3, ...)
-#       pass
-#   def __call__(self, t):
-#       # Evaluate this mean function at array of times t
 
 
 class MeanModel(object):
@@ -34,7 +25,37 @@ class MeanModel(object):
 
     @classmethod
     def initialize(cls):
+        """ Initialize instance, setting all parameters to 0. """
         return cls( *([0.]*cls._parsize) )
+
+    def __add__(self, b):
+        return Sum(self, b)
+    def __radd__(self, b):
+        return self.__add__(b)
+
+
+class Sum(MeanModel):
+    """ Sum of two mean functions """
+    def __init__(self, m1, m2):
+        self.m1, self.m2 = m1, m2
+
+    @property
+    def _parsize(self):
+        return self.m1._parsize + self.m2._parsize
+
+    @property
+    def pars(self):
+        return self.m1.pars + self.m2.pars
+
+    def initialize(self):
+        return
+
+    def __repr__(self):
+        return "{0} + {1}".format(self.m1, self.m2)
+
+    @array_input
+    def __call__(self, t):
+        return self.m1(t) + self.m2(t)
 
 
 class Constant(MeanModel):
@@ -45,7 +66,7 @@ class Constant(MeanModel):
 
     @array_input
     def __call__(self, t):
-        return self.pars[0] * np.ones_like(t)
+        return np.full(t.shape, self.pars[0])
 
 
 class Linear(MeanModel):
@@ -62,18 +83,32 @@ class Linear(MeanModel):
         return self.pars[0] * t + self.pars[1]
 
 
-class Parabola(object):
+class Parabola(MeanModel):
     """ 
     A 2nd degree polynomial mean function
     m(t) = quad * t**2 + slope * t + intercept 
     """
-    _parsize = 2
+    _parsize = 3
     def __init__(self, quad, slope, intercept):
-        pass
+        super(Parabola, self).__init__(quad, slope, intercept)
 
     @array_input
     def __call__(self, t):
-        return self.pars[0] * t**2 + self.pars[1] * t + self.pars[2]
+        return np.polyval(self.pars, t)
+
+
+class Cubic(MeanModel):
+    """ 
+    A 3rd degree polynomial mean function
+    m(t) = cub * t**3 + quad * t**2 + slope * t + intercept 
+    """
+    _parsize = 4
+    def __init__(self, cub, quad, slope, intercept):
+        super(Cubic, self).__init__(cub, quad, slope, intercept)
+
+    @array_input
+    def __call__(self, t):
+        return np.polyval(self.pars, t)
 
 
 class Keplerian(MeanModel):
@@ -86,7 +121,7 @@ class Keplerian(MeanModel):
     e = eccentricity
     K = RV amplitude in m/s 
     w = longitude of the periastron
-    T0 = zero phase
+    T0 = time of periastron passage
     
     RV = K[cos(w+v) + e*cos(w)] + sis_vel
     """
@@ -98,22 +133,22 @@ class Keplerian(MeanModel):
     def __call__(self, t):
         P, K, e, w, T0 = self.pars
         #mean anomaly
-        Mean_anom=[2*np.pi*(x1-T0)/P  for x1 in t]
+        Mean_anom = 2*np.pi*(t-T0)/P
         #eccentric anomaly -> E0=M + e*sin(M) + 0.5*(e**2)*sin(2*M)
-        E0=[x1 + e*np.sin(x1)  + 0.5*(e**2)*np.sin(2*x1) \
-                                                            for x1 in Mean_anom]
+        E0 = Mean_anom + e*np.sin(Mean_anom) + 0.5*(e**2)*np.sin(2*Mean_anom)
         #mean anomaly -> M0=E0 - e*sin(E0)
-        M0=[x1 - e*np.sin(x1) for x1 in E0]
+        M0 = E0 - e*np.sin(E0)
 
-        i=0
-        while i<100:
-            #[x + y for x, y in zip(first, second)]
-            calc_aux=[x2-y2 for x2,y2 in zip(Mean_anom,M0)]
-            E1=[x3 + y3/(1-e*np.cos(x3)) for x3,y3 in zip(E0,calc_aux)]
-            M1=[x4 - e*np.sin(x4) for x4 in E0]
-            i+=1
-            E0=E1
-            M0=M1
-        nu = [2*np.arctan(np.sqrt((1+e)/(1-e))*np.tan(x5/2)) for x5 in E0]
-        RV = [K*(e*np.cos(w)+np.cos(w+x6)) for x6 in nu]
+        niter=0
+        while niter < 100:
+            aux = Mean_anom - M0
+            E1 = E0 + aux/(1 - e*np.cos(E0))
+            M1 = E0 - e*np.sin(E0)
+
+            niter += 1
+            E0 = E1
+            M0 = M1
+
+        nu = 2*np.arctan(np.sqrt((1+e)/(1-e))*np.tan(E0/2))
+        RV = K*(e*np.cos(w)+np.cos(w+nu))
         return RV
