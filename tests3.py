8331e1767d412cef10740aeabff223aaad503be3
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-04-04 14:21:17 +0100

    tests woth jones framework

--- tests3.py
+++ tests3.py
@@ -14,22 +14,23 @@ t,rv, rvyerr, bis, rhk,sig_rhk = np.loadtxt("miniframe/datasets/HD41248_harps.rd
                                             unpack=True, usecols=(0,1,2,5,9,10))
 bis_err=2*rvyerr
 
-gpObj = SMALLgp.SMALLgp(kernels.SquaredExponential, [Constant, Constant, None], 3, t, rv, rvyerr, bis, bis_err, rhk, sig_rhk)
 
 # a = [l, a_01,a_02,a_03, a_11,a_12,a_13, ..., a_l1,a_l2,a_l3 ]
-#a = np.array([np.exp(1), np.exp(1),0,np.exp(1), np.exp(1),np.exp(1),np.exp(1), np.exp(1),np.exp(1),10*np.exp(1)])
-a = np.array([np.exp(1), np.exp(1),0,np.exp(1), np.exp(1),np.exp(1),np.exp(1)])
+gpObj = SMALLgp.SMALLgp(kernels.SquaredExponential, [Constant, Constant], 2, t, rv, rvyerr, bis, bis_err)
+a = np.array([1, 2,1,0.5, 1,2,3])
+b = [10, 5]
+print(gpObj.log_likelihood(a,b))
 
-a = np.array([np.exp(1), np.exp(1),0,np.exp(1), np.exp(1),np.exp(1),np.exp(1), 0,0,0])
-b = [10, 2]
+gpObj = SMALLgp.SMALLgp(kernels.SquaredExponential, [Constant, Constant, None], 3, t, rv, rvyerr, bis, bis_err, rhk, sig_rhk)
+a = np.array([1, 2,1,0.5, 1,2,3, 0,0,0])
+b = [10, 5]
+print(gpObj.log_likelihood(a,b))
+
 
 k11 = gpObj.kii(a = a, x = t, position = 1)
 k22 = gpObj.kii(a = a, x = t, position = 2)
 k33 = gpObj.kii(a = a, x = t, position = 3)
-
 k12 = gpObj.kij(a = a, x = t, position1 = 1, position2 = 2)
 k13 = gpObj.kij(a = a, x = t, position1 = 1, position2 = 3)
 k23 = gpObj.kij(a = a, x = t, position1 = 2, position2 = 3)
-
 K = gpObj.compute_matrix(a)
-print(gpObj.log_likelihood(a,b))
