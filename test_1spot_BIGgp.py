df5a590ce143c4aa2471495b015bc7d2d4abfc22
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-05-19 14:43:18 +0100

    keep the tests going

--- test_1spot_BIGgp.py
+++ test_1spot_BIGgp.py
@@ -3,7 +3,6 @@
 from miniframe import kernels
 from miniframe.BIGgp import BIGgp
 from miniframe.means import Constant, Linear, Keplerian
-from time import time
 
 import numpy as np
 import emcee
@@ -170,28 +169,32 @@ axes[4].set_xlabel("step number")
 fig.tight_layout(h_pad=0.0)
 plt.show()
 
-#time = np.linspace(0, 75, 500)
-#a = [ll1[0], ll2[0], pp[0], vcvc[0], vrvr[0], lclc[0], bcbc[0], brbr[0]]
-#mu1, std1 = gpObj.draw_from_gp(time, a,  model = 'rv')
-#mu2, std2 = gpObj.draw_from_gp(time, a,  model = 'bis')
-#mu3, std3 = gpObj.draw_from_gp(time, a,  model = 'rhk')
-#
-#f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
-#ax1.set_title(' ')
-#ax1.fill_between(time, mu1+std1, mu1-std1, color="grey", alpha=0.5)
-#ax1.plot(time, mu1, "k-", alpha=1, lw=1.5)
-#ax1.plot(t,rv,"b*")
-#ax1.set_ylabel("RVs")
-#
-#ax2.fill_between(time, mu2+std2, mu2-std2, color="grey", alpha=0.5)
-#ax2.plot(time, mu2, "k-", alpha=1, lw=1.5)
-#ax2.plot(t,bis,"b*")
-#ax2.set_ylabel("BIS")
-#
-#ax3.fill_between(time, mu3+std3, mu3-std3, color="grey", alpha=0.5)
-#ax3.plot(time, mu3, "k-", alpha=1, lw=1.5)
-#ax3.plot(t,rhk,"b*")
-#ax3.set_ylabel("flux")
-#ax3.set_xlabel("time")
-#plt.show()
+time = np.linspace(t.min(), t.max(), 1000)
+mu11, cov11, std11  = gpObj.predict_gp(time, a, b, model = 'rv')
+mu22, cov22, std22  = gpObj.predict_gp(time, a, b, model = 'bis')
+mu33, cov33, std33  = gpObj.predict_gp(time, a, b, model = 'rhk')
 
+f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+ax1.set_title(' ')
+ax1.fill_between(time, mu11+std11, mu11-std11, color="grey", alpha=0.5)
+ax1.plot(time, mu11, "k--", alpha=1, lw=1.5)
+ax1.plot(t,rv,"b.")
+ax1.set_ylabel("RVs")
+
+ax2.fill_between(time, mu33+std33, mu33-std33, color="grey", alpha=0.5)
+ax2.plot(time, mu33, "k--", alpha=1, lw=1.5)
+ax2.plot(t,rhk,"b.")
+ax2.set_ylabel("flux")
+
+ax3.fill_between(time, mu22+std22, mu22-std22, color="grey", alpha=0.5)
+ax3.plot(time, mu22, "k--", alpha=1, lw=1.5)
+ax3.plot(t,bis,"b.")
+ax3.set_ylabel("BIS")
+ax3.set_xlabel("time")
+plt.show()
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey = True, sharex = True)
+ax1.imshow(cov11)
+ax2.imshow(cov33)
+ax3.imshow(cov22)
+plt.show()
